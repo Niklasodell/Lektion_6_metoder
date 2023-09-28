0e8c4a64f60efc97524faa3801bcc526c31b5b4e
Niklasodell <144327353+Niklasodell@users.noreply.github.com>
2023-09-28 15:36:02 +0200

    Third commit metoder

--- src/com/niklas/metoder/Main.java
+++ src/com/niklas/metoder/Main.java
@@ -3,18 +3,23 @@ package com.niklas.metoder;
 public class Main {
     public static void main(String[] args) {
 
-        // Object Creation AKA Insantiation
-        Student benny = new Student(15, "Benny", false);
-        Student frida = new Student(25, "Frida", true);
+        int[] scores = {0, 150, 500, 5000};
 
-        System.out.println(benny.name);
-        System.out.println(benny.age);
-        System.out.println(benny.isTired);
+        printScores(scores);
+        checkScoreIfZero(scores);
+    }
 
-        System.out.println(frida.name);
-        System.out.println(frida.age);
-        System.out.println(frida.showIfSleepy());
+    public static void printScores(int[] scores) {
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(scores[i]);
+        }
+    }
 
-
+    public static void checkScoreIfZero(int[] scores) {
+        if (scores[0] == 0) {
+                System.out.println("The score was indeed 0");
+            } else {
+                System.out.println("The score wasn't 0");
+        }
     }
 }
